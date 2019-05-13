4e9a21f25612414d1910f379bd1ec01bbb2464a8
a.fialko <a.fialko@corp.mail.ru>
2019-05-13 09:58:06 +0300

    add inner container that will wrapping BottomSheet

--- bottomdrawer/src/main/java/com/github/heyalex/bottomdrawer/BottomDrawer.kt
+++ bottomdrawer/src/main/java/com/github/heyalex/bottomdrawer/BottomDrawer.kt
@@ -1,8 +1,27 @@
 package com.github.heyalex.bottomdrawer
 
 import android.content.Context
+import android.util.AttributeSet
+import android.view.ViewGroup
 import android.widget.FrameLayout
 
-class BottomDrawer(context: Context) : FrameLayout(context) {
+class BottomDrawer : FrameLayout {
+    private var container: FrameLayout
 
+    constructor(context: Context) : this(context, null)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int)
+        : super(context, attrs, defStyleAttr) {
+
+        container = FrameLayout(context).apply {
+            val params =
+                FrameLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+                )
+
+            layoutParams = params
+        }
+    }
 }
