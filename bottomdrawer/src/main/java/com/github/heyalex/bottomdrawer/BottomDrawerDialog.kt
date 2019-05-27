9275879a5bce34526e77d51c20c597bc0ecb6822
a.fialko <a.fialko@corp.mail.ru>
2019-05-27 23:04:07 +0300

    add flags window insets

--- bottomdrawer/src/main/java/com/github/heyalex/bottomdrawer/BottomDrawerDialog.kt
+++ bottomdrawer/src/main/java/com/github/heyalex/bottomdrawer/BottomDrawerDialog.kt
@@ -1,11 +1,16 @@
 package com.github.heyalex.bottomdrawer
 
 import android.content.Context
+import android.graphics.Color
+import android.os.Build
+import android.os.Bundle
 import android.support.annotation.StyleRes
 import android.support.design.widget.BottomSheetBehavior
 import android.support.design.widget.CoordinatorLayout
 import android.support.v7.app.AppCompatDialog
+import android.view.View
 import android.view.Window
+import android.view.WindowManager
 
 class BottomDrawerDialog(context: Context, @StyleRes theme: Int = R.style.BottomDialogTheme) :
     AppCompatDialog(context, theme) {
@@ -19,4 +24,28 @@ class BottomDrawerDialog(context: Context, @StyleRes theme: Int = R.style.Bottom
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window?.let {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                it.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                it.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            }
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                it.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                it.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
+                it.statusBarColor = Color.TRANSPARENT
+
+                var flags = it.decorView.systemUiVisibility
+                flags =
+                    flags xor View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                it.decorView.systemUiVisibility = flags
+            }
+        }
+    }
 }
