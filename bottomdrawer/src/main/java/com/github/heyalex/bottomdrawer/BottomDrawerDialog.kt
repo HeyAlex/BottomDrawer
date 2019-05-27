0e20de2a6f588c860d7f4482947e530d31f342a8
a.fialko <a.fialko@corp.mail.ru>
2019-05-27 23:17:41 +0300

    wrapping bottom sheet like in design library

--- bottomdrawer/src/main/java/com/github/heyalex/bottomdrawer/BottomDrawerDialog.kt
+++ bottomdrawer/src/main/java/com/github/heyalex/bottomdrawer/BottomDrawerDialog.kt
@@ -4,13 +4,19 @@ import android.content.Context
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
+import android.support.annotation.LayoutRes
 import android.support.annotation.StyleRes
 import android.support.design.widget.BottomSheetBehavior
 import android.support.design.widget.CoordinatorLayout
+import android.support.v4.view.AccessibilityDelegateCompat
+import android.support.v4.view.ViewCompat
+import android.support.v4.view.accessibility.AccessibilityNodeInfoCompat
 import android.support.v7.app.AppCompatDialog
 import android.view.View
+import android.view.ViewGroup
 import android.view.Window
 import android.view.WindowManager
+import android.widget.FrameLayout
 
 class BottomDrawerDialog(context: Context, @StyleRes theme: Int = R.style.BottomDialogTheme) :
     AppCompatDialog(context, theme) {
@@ -47,5 +53,98 @@ class BottomDrawerDialog(context: Context, @StyleRes theme: Int = R.style.Bottom
                 it.decorView.systemUiVisibility = flags
             }
         }
+    }
+
+    override fun setContentView(@LayoutRes layoutResId: Int) {
+        super.setContentView(wrapInBottomSheet(layoutResId, null, null))
+    }
+
+    override fun setContentView(view: View) {
+        super.setContentView(wrapInBottomSheet(0, view, null))
+    }
+
+    override fun setContentView(view: View, params: ViewGroup.LayoutParams?) {
+        super.setContentView(wrapInBottomSheet(0, view, params))
+    }
+
+    private fun wrapInBottomSheet(
+        layoutResId: Int,
+        view: View?,
+        params: ViewGroup.LayoutParams?
+    ): View {
+        var wrappedView = view
+        val container = View.inflate(context, R.layout.bottom_drawer_layout, null) as FrameLayout
+        coordinator = container.findViewById(R.id.bottom_sheet_coordinator)
+        if (layoutResId != 0 && wrappedView == null) {
+            wrappedView = layoutInflater.inflate(layoutResId, coordinator, false)
+        }
+        drawer = coordinator.findViewById<View>(R.id.bottom_sheet_drawer) as BottomDrawer
+
+        if (params == null) {
+            drawer.addView(wrappedView)
+        } else {
+            drawer.addView(wrappedView, params)
+        }
+
+        behavior?.setBottomSheetCallback(object :
+            BottomSheetBehavior.BottomSheetCallback() {
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+
+            }
+
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                when (newState) {
+                    BottomSheetBehavior.STATE_COLLAPSED -> {
+
+                    }
+                    BottomSheetBehavior.STATE_HIDDEN -> {
+                        dismiss()
+                    }
+                    BottomSheetBehavior.STATE_EXPANDED -> {
+
+                    }
+                    BottomSheetBehavior.STATE_DRAGGING -> {
+
+                    }
+                    BottomSheetBehavior.STATE_SETTLING -> {
+
+                    }
+                }
+            }
+        })
+
+        coordinator.findViewById<View>(R.id.touch_outside)
+            .setOnClickListener {
+                behavior?.state = BottomSheetBehavior.STATE_HIDDEN
+            }
+
+        // Handle accessibility events
+        ViewCompat.setAccessibilityDelegate(
+            drawer,
+            object : AccessibilityDelegateCompat() {
+                override fun onInitializeAccessibilityNodeInfo(
+                    host: View, info: AccessibilityNodeInfoCompat
+                ) {
+                    super.onInitializeAccessibilityNodeInfo(host, info)
+                    info.isDismissable = true
+                }
+
+                override fun performAccessibilityAction(
+                    host: View,
+                    action: Int,
+                    args: Bundle
+                ): Boolean {
+                    if (action == AccessibilityNodeInfoCompat.ACTION_DISMISS) {
+                        cancel()
+                        return true
+                    }
+                    return super.performAccessibilityAction(host, action, args)
+                }
+            })
+        drawer.setOnTouchListener { view, event ->
+            // Consume the event and prevent it from falling through
+            true
+        }
+        return container
     }
 }
