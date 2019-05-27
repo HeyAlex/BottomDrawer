5c5fbf72012c757dbc24cd5a342c00aff2c2c5f1
a.fialko <a.fialko@corp.mail.ru>
2019-05-27 22:49:11 +0300

    example

--- app/src/main/java/com/github/heyalex/bottomdrawerexample/MainActivity.kt
+++ app/src/main/java/com/github/heyalex/bottomdrawerexample/MainActivity.kt
@@ -2,11 +2,16 @@ package com.github.heyalex.bottomdrawerexample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        show_button.setOnClickListener {
+
+        }
+
     }
 }
