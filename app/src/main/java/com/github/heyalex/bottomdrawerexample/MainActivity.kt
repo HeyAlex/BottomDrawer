9d6d83d43c7fbd3b14c9c3bb36a9380b67d1db2b
a.fialko <a.fialko@corp.mail.ru>
2019-09-15 16:24:21 +0300

    show drawer outside of dialog

--- app/src/main/java/com/github/heyalex/bottomdrawerexample/MainActivity.kt
+++ app/src/main/java/com/github/heyalex/bottomdrawerexample/MainActivity.kt
@@ -4,15 +4,28 @@ import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import com.github.heyalex.bottomdrawer.BottomDrawer
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
+    internal var behavior: BottomSheetBehavior<BottomDrawer>? = null
+    internal lateinit var drawer: BottomDrawer
+    private lateinit var coordinator: CoordinatorLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_test)
+        coordinator = findViewById(R.id.bottom_sheet_coordinator)
+        drawer = coordinator.findViewById(R.id.bottom_sheet_drawer) as BottomDrawer
+        behavior = BottomSheetBehavior.from(drawer)
+
         window?.let {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
                 it.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
@@ -22,19 +35,32 @@ class MainActivity : AppCompatActivity() {
             }
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                it.statusBarColor = Color.WHITE
+                it.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                it.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
+                it.statusBarColor = Color.TRANSPARENT
+
+                var flags = it.decorView.systemUiVisibility
+                flags =
+                    flags xor View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    flags =
+                        flags xor View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+                }
+                it.decorView.systemUiVisibility = flags
             }
         }
 
-        setContentView(R.layout.activity_main)
         show_button.setOnClickListener {
             supportFragmentManager?.beginTransaction()?.add(GoogleTaskExampleDialog(), "test")
                 ?.commit()
         }
 
         show_button_sample.setOnClickListener {
-            supportFragmentManager?.beginTransaction()?.add(ExampleDialog(), "test1")
-                ?.commit()
+//            supportFragmentManager?.beginTransaction()?.add(ExampleDialog(), "test1")
+//                ?.commit()
+            behavior?.state = BottomSheetBehavior.STATE_HALF_EXPANDED
+
         }
     }
 }
