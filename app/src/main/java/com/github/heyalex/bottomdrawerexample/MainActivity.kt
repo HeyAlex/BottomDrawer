e44ff9e2c09c465be3fa0fc2be1d1f79f464b414
a.fialko <a.fialko@corp.mail.ru>
2019-10-05 13:13:45 +0300

    clean up sample

--- app/src/main/java/com/github/heyalex/bottomdrawerexample/MainActivity.kt
+++ app/src/main/java/com/github/heyalex/bottomdrawerexample/MainActivity.kt
@@ -4,54 +4,44 @@ import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.view.View
-import android.view.Window
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.coordinatorlayout.widget.CoordinatorLayout
-import com.github.heyalex.bottomdrawer.BottomDrawer
-import com.google.android.material.bottomsheet.BottomSheetBehavior
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
-    internal var behavior: BottomSheetBehavior<BottomDrawer>? = null
-    internal lateinit var drawer: BottomDrawer
-    private lateinit var coordinator: CoordinatorLayout
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        requestWindowFeature(Window.FEATURE_NO_TITLE)
-        setContentView(R.layout.activity_main_test)
-        coordinator = findViewById(R.id.bottom_sheet_coordinator)
-        drawer = coordinator.findViewById(R.id.bottom_sheet_drawer) as BottomDrawer
-        behavior = BottomSheetBehavior.from(drawer)
+        setContentView(R.layout.activity_main)
 
-//        drawer.addView(wrappedView)
-//        drawer.addHandleView(handleView)
-
-        window?.let {
+        window.let {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                it.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+                window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
             }
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                it.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+                // for drawing behind status bar
+                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
             }
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                it.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                //make system bar to be translucent
+                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                it.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
-                it.statusBarColor = Color.TRANSPARENT
+
+                //make status bar color transparent
+                window.statusBarColor = Color.TRANSPARENT
 
                 var flags = it.decorView.systemUiVisibility
+                // make dark status bar icons
                 flags =
                     flags xor View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    // make dark navigation bar icons
                     flags =
                         flags xor View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
                 }
-                it.decorView.systemUiVisibility = flags
+                window.decorView.systemUiVisibility = flags
             }
         }
 
@@ -61,10 +51,8 @@ class MainActivity : AppCompatActivity() {
         }
 
         show_button_sample.setOnClickListener {
-            //            supportFragmentManager?.beginTransaction()?.add(ExampleDialog(), "test1")
-//                ?.commit()
-            behavior?.state = BottomSheetBehavior.STATE_HALF_EXPANDED
-
+            supportFragmentManager?.beginTransaction()?.add(CustomExampleDialog(), "test1")
+                ?.commit()
         }
     }
 }
