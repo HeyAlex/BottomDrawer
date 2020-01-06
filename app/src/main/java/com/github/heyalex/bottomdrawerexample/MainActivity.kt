2c4ef65f58ee543e80c1ec4bd13753a3e1a03911
a.fialko <a.fialko@corp.mail.ru>
2020-01-07 01:23:23 +0300

    update example

--- app/src/main/java/com/github/heyalex/bottomdrawerexample/MainActivity.kt
+++ app/src/main/java/com/github/heyalex/bottomdrawerexample/MainActivity.kt
@@ -14,36 +14,36 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        window.let {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-            }
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                // for drawing behind status bar
-                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-            }
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                //make system bar to be translucent
-                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-
-                //make status bar color transparent
-                window.statusBarColor = Color.TRANSPARENT
-
-                var flags = it.decorView.systemUiVisibility
-                // make dark status bar icons
-                flags =
-                    flags xor View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    // make dark navigation bar icons
-                    flags =
-                        flags xor View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
-                }
-                window.decorView.systemUiVisibility = flags
-            }
-        }
+//
+//        window.let {
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+//                window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+//            }
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//                // for drawing behind status bar
+//                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+//            }
+//
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//                //make system bar to be translucent
+//                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+//                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//
+//                //make status bar color transparent
+//                window.statusBarColor = Color.TRANSPARENT
+//
+//                var flags = it.decorView.systemUiVisibility
+//                // make dark status bar icons
+//                flags =
+//                    flags xor View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//                    // make dark navigation bar icons
+//                    flags =
+//                        flags xor View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+//                }
+//                window.decorView.systemUiVisibility = flags
+//            }
+//        }
 
         show_button.setOnClickListener {
             supportFragmentManager?.beginTransaction()?.add(GoogleTaskExampleDialog(), "test")
